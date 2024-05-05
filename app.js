6de69a15e704f6252243c32c4a80480156d33536
Azadas26 <azad45518@gmail.com>
2024-05-05 21:15:40 +0530

    Database Connected Successfully

---
+++ app.js
@@ -0,0 +1,52 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var hbs = require('express-handlebars')
+var db = require('./connection/connect')
+
+var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layouts/',partialsDir:__dirname+'/views/partials/'}))
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+db.Database_Connection().then((resc)=>
+{
+  console.log(resc);
+}).catch((err)=>
+{
+   console.log(err);
+})
+
+app.use('/', usersRouter);
+app.use('/users', adminRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ connection/connect.js
@@ -0,0 +1,34 @@
+var promise = require('promise');
+var mongoClient = require('mongodb').MongoClient;
+
+var state =
+{
+    db : null
+}
+module.exports =
+{
+    Database_Connection : ()=>
+    {
+        return new promise((resolve,reject)=>
+        {
+            mongoClient.connect('mongodb://localhost:27017',{ useNewUrlParser: true, useUnifiedTopology: true },(err,data)=>
+            {
+                var dbname = "Ticket_Validation"
+                if(err)
+                {
+                    reject(err)
+                }
+                else
+                {
+                    state.db = data.db(dbname);
+                    resolve("Databse Connection Successfull...")
+                }
+            })
+        })
+    }
+}
+
+var get = ()=>
+{
+    return state.db;
+}
