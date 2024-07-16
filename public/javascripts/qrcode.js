48346e809436cf8b8e6e1e673af5a68332557564
Azadas26 <azad45518@gmail.com>
2024-07-16 20:53:49 +0530

    Public Server setup

--- public/javascripts/qrcode.js
+++ public/javascripts/qrcode.js
@@ -12,7 +12,9 @@ module.exports =
             // Data to be encoded in the QR code
 
             // Data to be encoded in the QR code
-            const data = 'http://localhost:3000/checker/busticket?id='+Id; // Replace with your own data
+            let local =  'http://localhost:3000'
+            let ngrock = 'https://ba38-103-149-158-207.ngrok-free.app'
+            const data = `${local}/checker/busticket?id=${Id}`; // Replace with your own data
 
             // Options for generating the QR code
             const options = {
